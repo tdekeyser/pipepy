ee70042973b9a6560029f11bdb45b13e2da3e451
tdekeyser <tom_dekeyser_7@hotmail.com>
2018-06-20 21:52:51 +0200

    new AddColumnPipe + test

--- pipepy/pandas_pipe.py
+++ pipepy/pandas_pipe.py
@@ -8,7 +8,7 @@ import pandas as pd
 from pipepy.core import Pipe, ResidueMixin
 
 
-def category_to_numeric(vector: pd.Categorical) -> (pd.Categorical, pd.Categorical):
+def category_to_numeric(vector: pd.Categorical):
     """
     Transform a categorical vector of data into numeric bins. The function
     returns a duplicate and the original categories.
@@ -27,7 +27,7 @@ def category_to_numeric(vector: pd.Categorical) -> (pd.Categorical, pd.Categoric
     array([0, 1, 2, 0])
     """
     uniques = [x for x in pd.unique(vector) if not pd.isna(x)]
-    return pd.Categorical(vector).rename_categories(range(len(uniques))), uniques
+    return pd.to_numeric(pd.Categorical(vector).rename_categories(range(len(uniques)))), uniques
 
 
 class CategoryToNumericPipe(ResidueMixin, Pipe):
@@ -65,9 +65,35 @@ class DropColumnPipe(Pipe):
         return data
 
 
+class AddColumnPipe(Pipe):
+    """
+    Add columns to data.
+
+    :param cols: Columns to add
+    :param names: Respective names for the new columns
+    """
+
+    def __init__(self, columns, names):
+        assert len(columns) == len(names), \
+            'Length of columns must be the same as the column names: %i - %i' % (len(columns), len(names))
+
+        self.__columns = columns
+        self.__names = names
+
+    def flush(self, data: pd.DataFrame) -> pd.DataFrame:
+        num_rows = data.shape[0]
+
+        for i, col in enumerate(self.__columns):
+            assert num_rows == col.shape[0]
+            data[self.__names[i]] = col
+
+        return data
+
+
 class MapColumnPipe(Pipe):
     """
-    Apply function func on requested columns.
+    Apply function func on requested columns. If requested columns is None,
+    the function will be applied on all columns.
 
     :param func: Function to be applied
     :param columns: Column names or indices to apply func
@@ -83,4 +109,25 @@ class MapColumnPipe(Pipe):
         columns = self.__columns if self.__columns else data.columns
         for col in columns:
             data[col] = data[col].map(self.__map_func)
+        return data
+
+
+class PeekPipe(Pipe):
+    """
+    Allows to perform actions on the data that does not alter
+    it in any way. Suitable for data printing or inspection.
+
+    This pipe always performs :peek_func: on a copy of the
+    data. For very large datasets, this pipe is not recommended!
+
+    :param peek_func: Function to apply on data
+    """
+
+    def __init__(self, peek_func: Callable):
+        assert callable(peek_func), 'Peek function should be callable.'
+
+        self.__peek_func = peek_func
+
+    def flush(self, data: pd.DataFrame) -> pd.DataFrame:
+        self.__peek_func(data.copy())
         return data
--- tests/test__pandas_pipe.py
+++ tests/test__pandas_pipe.py
@@ -4,7 +4,7 @@ import numpy as np
 import pandas as pd
 from numpy.testing import assert_array_equal
 
-from pipepy.pandas_pipe import category_to_numeric, CategoryToNumericPipe, DropColumnPipe, MapColumnPipe
+from pipepy.pandas_pipe import category_to_numeric, CategoryToNumericPipe, DropColumnPipe, MapColumnPipe, AddColumnPipe
 
 
 class CategoryToNumericPipeTest(unittest.TestCase):
@@ -12,23 +12,38 @@ class CategoryToNumericPipeTest(unittest.TestCase):
     def test_category_to_numeric(self):
         transformed_array, residue = category_to_numeric(pd.Categorical(['A', 'b', 'c', 'A']))
 
-        assert_array_equal(transformed_array,pd.Categorical([0, 1, 2, 0]))
+        assert_array_equal(transformed_array, pd.Categorical([0, 1, 2, 0]))
         self.assertEqual(residue, ['A', 'b', 'c'])
 
     def test_category_to_numeric_pipe(self):
-        df = pd.DataFrame(np.asarray([[0, 1, 2, 3], [4, 5, 6, 7]]))
+        df = pd.DataFrame(np.asarray([
+            [0, 1, 2, 3],
+            [4, 5, 6, 7]
+        ]))
         pipe = CategoryToNumericPipe([0, 2])
 
-        assert_array_equal(pipe.flush(df), np.asarray([[0, 1, 0, 3], [1, 5, 1, 7]]))
+        assert_array_equal(pipe.flush(df), np.asarray([
+            [0, 1, 0, 3],
+            [1, 5, 1, 7]
+        ]))
 
     def test_category_to_numeric_pipe_colnames(self):
-        df = pd.DataFrame(np.asarray([[0, 1, 2, 3], [4, 5, 6, 7]]), columns=['one', 'two', 'three', 'four'])
+        df = pd.DataFrame(np.asarray([
+            [0, 1, 2, 3],
+            [4, 5, 6, 7]
+        ]), columns=['one', 'two', 'three', 'four'])
         pipe = CategoryToNumericPipe(['one', 'three'])
 
-        assert_array_equal(pipe.flush(df), np.asarray([[0, 1, 0, 3], [1, 5, 1, 7]]))
+        assert_array_equal(pipe.flush(df), np.asarray([
+            [0, 1, 0, 3],
+            [1, 5, 1, 7]
+        ]))
 
     def test_category_to_numeric_pipe_residue(self):
-        df = pd.DataFrame(np.asarray([[0, 1, 2, 3], [4, 5, 6, 7]]))
+        df = pd.DataFrame(np.asarray([
+            [0, 1, 2, 3],
+            [4, 5, 6, 7]
+        ]))
         pipe = CategoryToNumericPipe([0, 2])
 
         pipe.flush(df)
@@ -39,22 +54,77 @@ class CategoryToNumericPipeTest(unittest.TestCase):
 class DropColumnPipeTest(unittest.TestCase):
 
     def test_drop_columns(self):
-        df = pd.DataFrame(np.asarray([[0, 1, 2, 3], [4, 5, 6, 7]]))
+        df = pd.DataFrame(np.asarray([
+            [0, 1, 2, 3],
+            [4, 5, 6, 7]
+        ]))
         pipe = DropColumnPipe([0, 2, 3])
 
-        assert_array_equal(pipe.flush(df), np.asarray([[1], [5]]))
+        assert_array_equal(pipe.flush(df), np.asarray([
+            [1],
+            [5]
+        ]))
 
     def test_drop_columns_colnames(self):
-        df = pd.DataFrame(np.asarray([[0, 1, 2, 3], [4, 5, 6, 7]]), columns=['one', 'two', 'three', 'four'])
+        df = pd.DataFrame(np.asarray([
+            [0, 1, 2, 3],
+            [4, 5, 6, 7]
+        ]), columns=['one', 'two', 'three', 'four'])
         pipe = DropColumnPipe(['one', 'three'])
 
-        assert_array_equal(pipe.flush(df), np.asarray([[1, 3], [5, 7]]))
+        assert_array_equal(pipe.flush(df), np.asarray([
+            [1, 3],
+            [5, 7]
+        ]))
+
+
+class AddColumnPipeTest(unittest.TestCase):
+
+    def test_add_column(self):
+        df = pd.DataFrame(np.asarray([
+            [0, 1, 2, 3],
+            [4, 5, 6, 7]
+        ]))
+        pipe = AddColumnPipe([np.asarray([
+            [10],
+            [11]
+        ]), np.asarray([
+            [100],
+            [101]]
+        )], ['one', 'two'])
+
+        result = pipe.flush(df)
+
+        self.assertIn('one', result.columns)
+        self.assertIn('two', result.columns)
+        assert_array_equal(result, np.asarray([
+            [0, 1, 2, 3, 10, 100],
+            [4, 5, 6, 7, 11, 101]
+        ]))
+
+    def test_add_column_rows_not_equal_throws(self):
+        df = pd.DataFrame(np.asarray([
+            [0, 1, 2, 3],
+            [4, 5, 6, 7]
+        ]))
+        pipe = AddColumnPipe([np.asarray([
+            [10],
+        ])], ['mistake'])
+
+        with self.assertRaises(AssertionError):
+            pipe.flush(df)
 
 
 class ApplyOnAxisPipeTest(unittest.TestCase):
 
     def test_apply_on_axis(self):
-        df = pd.DataFrame(np.asarray([[0, 1, 2, 3], [4, 5, 6, 7]]), columns=['one', 'two', 'three', 'four'])
+        df = pd.DataFrame(np.asarray([
+            [0, 1, 2, 3],
+            [4, 5, 6, 7]
+        ]), columns=['one', 'two', 'three', 'four'])
         pipe = MapColumnPipe(lambda col: col + 1, columns=['one', 'four'])
 
-        assert_array_equal(pipe.flush(df), np.asarray([[1, 1, 2, 4], [5, 5, 6, 8]]))
+        assert_array_equal(pipe.flush(df), np.asarray([
+            [1, 1, 2, 4],
+            [5, 5, 6, 8]
+        ]))
