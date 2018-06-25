a11eeeb9ac26c2ea53a176804f51d751d253c6b1
tdekeyser <tom_dekeyser_7@hotmail.com>
2018-06-25 14:16:31 +0200

    add RemoveOutliersPipe

--- pipepy/clean.py
+++ pipepy/clean.py
@@ -10,17 +10,24 @@ class DropColumnPipe(ResidueMixin, Pipe):
     Drop columns from the DataFrame.
 
     :param cols: iterable of column names or indices
+    :param keep: If True, drops all except cols
     """
 
-    def __init__(self, columns: Sequence):
+    def __init__(self, columns: Sequence, keep=False):
         super().__init__()
         self.__columns = columns
+        self.__keep = keep
 
     def flush(self, data: pd.DataFrame) -> pd.DataFrame:
-        for col in self.__columns:
+        for col in self.__filter_columns(data.columns):
             self.add_residue(data[col])
             data = data.drop(col, axis='columns')
         return data
+
+    def __filter_columns(self, data_cols):
+        if self.__keep:
+            return list(filter(lambda col: col not in self.__columns, data_cols))
+        return self.__columns
 
 
 def category_to_numeric(vector: pd.Categorical):
@@ -50,24 +57,36 @@ class CategoryToNumericPipe(ResidueMixin, Pipe):
     Transform specified columns of a dataset into bins. Does not create
     a duplicate.
 
-    :param cols: iterable of column names or indices
+    `.residue` field contains the categories of the transformed columns.
+
+    :param cols: iterable of column names or indices. If cols is None,
+    all columns will be transformed.
     """
 
-    def __init__(self, columns):
+    def __init__(self, columns: Sequence = None, excludes=False):
         super().__init__()
         self.__columns = columns
+        self.__excludes = excludes
 
     def flush(self, data: pd.DataFrame) -> pd.DataFrame:
-        for col in self.__columns:
+        for col in self.__filter_columns(data.columns):
             data[col], residue = category_to_numeric(data[col])
             self.add_residue((col, residue))
         return data
+
+    def __filter_columns(self, data_cols):
+        if self.__excludes:
+            return list(filter(lambda col: col not in self.__columns, data_cols))
+        return self.__columns if self.__columns is not None else data_cols
 
 
 class VariableToBinPipe(ResidueMixin, Pipe):
     """
     Transform a continuous variable into discrete intervals (bins).
     If columns is None, all columns are transformed.
+
+    `.residue` field contains the categories of the transformed
+    variables.
     """
 
     def __init__(self, bins: int = 3, columns: Sequence = None):
@@ -81,5 +100,28 @@ class VariableToBinPipe(ResidueMixin, Pipe):
         columns = self.__columns if self.__columns is not None else data.columns
         for col in columns:
             data[col], categories = pd.cut(data[col], bins=self.__bins, labels=False, retbins=True)
-            self.add_residue(categories)
+            self.add_residue((col, categories))
+        return data
+
+
+class RemoveOutliersPipe(ResidueMixin, Pipe):
+    """
+    Remove rows that contain outliers, i.e. values greater than 3*std.
+
+    `.residue` field contains indices of removed rows.
+
+    :param columns: iterable of column names or indices. If cols is None,
+    all columns will be transformed.
+    """
+
+    def __init__(self, columns: Sequence = None):
+        super().__init__()
+        self.__columns = columns
+
+    def flush(self, data: pd.DataFrame) -> pd.DataFrame:
+        columns = self.__columns if self.__columns is not None else data.columns
+        for col in columns:
+            outlier_index = data[(data[col] - data[col].mean()).abs() > 3 * data[col].std()]
+            self.add_residue(outlier_index)
+            data = data.drop(outlier_index.index)
         return data
--- tests/test__clean.py
+++ tests/test__clean.py
@@ -4,37 +4,53 @@ import numpy as np
 import pandas as pd
 from numpy.testing import assert_array_equal
 
-from pipepy.clean import category_to_numeric, CategoryToNumericPipe, DropColumnPipe
+from pipepy.clean import category_to_numeric, CategoryToNumericPipe, DropColumnPipe, RemoveOutliersPipe
 
 
 class DropColumnPipeTest(unittest.TestCase):
 
-    def test_drop_columns(self):
-        df = pd.DataFrame(np.asarray([
+    def setUp(self):
+        self.df = pd.DataFrame(np.asarray([
             [0, 1, 2, 3],
             [4, 5, 6, 7]
         ]))
+
+    def test_drop(self):
         pipe = DropColumnPipe([0, 2, 3])
 
-        assert_array_equal(pipe.flush(df), np.asarray([
+        assert_array_equal(pipe.flush(self.df), np.asarray([
             [1],
             [5]
         ]))
 
-    def test_drop_columns_colnames(self):
-        df = pd.DataFrame(np.asarray([
+    def test_use_colnames(self):
+        self.df = pd.DataFrame(np.asarray([
             [0, 1, 2, 3],
             [4, 5, 6, 7]
         ]), columns=['one', 'two', 'three', 'four'])
         pipe = DropColumnPipe(['one', 'three'])
 
-        assert_array_equal(pipe.flush(df), np.asarray([
+        assert_array_equal(pipe.flush(self.df), np.asarray([
             [1, 3],
             [5, 7]
         ]))
 
+    def test_keep(self):
+        pipe = DropColumnPipe([0, 2, 3], keep=True)
+
+        assert_array_equal(pipe.flush(self.df), np.asarray([
+            [0, 2, 3],
+            [4, 6, 7]
+        ]))
+
 
 class CategoryToNumericPipeTest(unittest.TestCase):
+
+    def setUp(self):
+        self.df = pd.DataFrame(np.asarray([
+            [0, 1, 2, 3],
+            [0, 5, 6, 7]
+        ]))
 
     def test_category_to_numeric(self):
         transformed_array, residue = category_to_numeric(pd.Categorical(['A', 'b', 'c', 'A']))
@@ -42,40 +58,62 @@ class CategoryToNumericPipeTest(unittest.TestCase):
         assert_array_equal(transformed_array, pd.Categorical([0, 1, 2, 0]))
         self.assertEqual(residue, ['A', 'b', 'c'])
 
-    def test_category_to_numeric_pipe(self):
-        df = pd.DataFrame(np.asarray([
-            [0, 1, 2, 3],
-            [4, 5, 6, 7]
-        ]))
+    def test_pipe(self):
         pipe = CategoryToNumericPipe([0, 2])
 
-        assert_array_equal(pipe.flush(df), np.asarray([
+        assert_array_equal(pipe.flush(self.df), np.asarray([
             [0, 1, 0, 3],
-            [1, 5, 1, 7]
+            [0, 5, 1, 7]
         ]))
 
-    def test_category_to_numeric_pipe_colnames(self):
-        df = pd.DataFrame(np.asarray([
+    def test_use_colnames(self):
+        self.df = pd.DataFrame(np.asarray([
             [0, 1, 2, 3],
             [4, 5, 6, 7]
         ]), columns=['one', 'two', 'three', 'four'])
         pipe = CategoryToNumericPipe(['one', 'three'])
 
-        assert_array_equal(pipe.flush(df), np.asarray([
+        assert_array_equal(pipe.flush(self.df), np.asarray([
             [0, 1, 0, 3],
             [1, 5, 1, 7]
         ]))
 
-    def test_category_to_numeric_pipe_residue(self):
-        df = pd.DataFrame(np.asarray([
-            [0, 1, 2, 3],
-            [4, 5, 6, 7]
+    def test_no_input_transforms_all(self):
+        pipe = CategoryToNumericPipe()
+
+        assert_array_equal(pipe.flush(self.df), np.asarray([
+            [0, 0, 0, 0],
+            [0, 1, 1, 1]
         ]))
+
+    def test_excludes(self):
+        pipe = CategoryToNumericPipe([2], excludes=True)
+
+        assert_array_equal(pipe.flush(self.df), np.asarray([
+            [0, 0, 2, 0],
+            [0, 1, 6, 1]
+        ]))
+
+    def test_residue(self):
         pipe = CategoryToNumericPipe([0, 2])
 
-        pipe.flush(df)
+        pipe.flush(self.df)
 
-        self.assertEqual(pipe.residue, [(0, [0, 4]), (2, [2, 6])])
+        self.assertEqual(pipe.residue, [(0, [0]), (2, [2, 6])])
+
+
+class RemoveOutliersPipeTest(unittest.TestCase):
+
+    def test_removes_outliers(self):
+        df = pd.DataFrame(np.zeros((100, 3)))
+        df.iloc[10, 0] = 10000
+        pipe = RemoveOutliersPipe()
+
+        result = pipe.flush(df)
+
+        self.assertEqual(result.shape, (99, 3))
+        assert_array_equal(result.sum(), np.asarray([0, 0, 0]))
+        assert_array_equal(pipe.residue[0], np.asarray([[10000, 0, 0]]))
 
 
 class VariableToBinPipeTest(unittest.TestCase):
