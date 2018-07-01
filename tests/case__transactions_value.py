b72c7c41f51bd796f5f8749ffb1908baf0614488
tdekeyser <tom_dekeyser_7@hotmail.com>
2018-07-01 16:33:32 +0200

    transactions first model try-out

--- tests/case__transactions_value.py
+++ tests/case__transactions_value.py
@@ -1,51 +1,16 @@
-import dask.dataframe as ddf
-import matplotlib.pyplot as plt
-import numpy as np
-import pandas as pd
-import scipy
-from sklearn.decomposition import IncrementalPCA, TruncatedSVD
-from sklearn.ensemble import GradientBoostingRegressor
-from sklearn.model_selection import cross_val_score
 from sklearn.preprocessing import MinMaxScaler
 
-from pipepy.clean import DropColumnPipe
 from pipepy.core import Pipeline
 from pipepy.engineer import MapColumnPipe
-from pipepy.explore import PeekPipe
 
 TRANSACTIONS_TRAIN = 'data/transactions-value/train.csv'
-TRANSACTIONS_TRAIN_INCPCA_700 = 'data/transactions-value/train_incPCA_700.h5'
 TRANSACTIONS_TEST = 'data/transactions-value/test.csv'
-
-
-def fit_model(data, model):
-    model = model.fit(data)
-
-    plt.title('Eigenvalue analysis')
-    plt.plot(model.explained_variance_ratio_.cumsum())
-    plt.show()
-
-    return model.transform(data)
-
-
-def plot_embedding(data):
-    plt.scatter(data[:, 0], data[:, 1], s=1)
-    plt.show()
 
 
 def fe_pipeline():
     return Pipeline([
-
-        PeekPipe(lambda d: print(d.columns[:10])),
-
-        DropColumnPipe(['ID', 'target']),
-
-        #MapColumnPipe(lambda col: MinMaxScaler().fit_transform(col.reshape(-1, 1))),
-
-        # lambda data: umap.UMAP(n_components=2, metric='correlation', verbose=1).fit_transform(data),
-
-        lambda data: fit_model(data, IncrementalPCA(n_components=700)),  # 98% variance
-        #plot_embedding,
+        MapColumnPipe(lambda col: MinMaxScaler().fit_transform(col)),
+        #lambda data: umap.UMAP(n_components=2, metric='correlation', verbose=1).fit_transform(data),
     ])
 
 
@@ -53,22 +18,36 @@ if __name__ == '__main__':
     # data = ddf.read_csv(TRANSACTIONS_TRAIN)
     # data = data.map_partitions(lambda part: part.to_sparse(fill_value=0))
     # data = data.compute().reset_index(drop=True)
-    data = pd.read_csv(TRANSACTIONS_TRAIN)
+    #data = pd.read_csv(TRANSACTIONS_TRAIN)
 
     #print(data.density)
 
-    # pipeline = fe_pipeline()
-    # reduced_data = pipeline.flush(data)
-    # reduced_data.to_hdf(TRANSACTIONS_TRAIN_INCPCA_700, 'TRANSACTIONS_TRAIN_INCPCA_700')
-    print('getting train/test')
+    import scipy
+    import pandas as pd
+    import numpy as np
+    import seaborn as sns
+    sns.set(style="white")
 
-    X = pd.read_hdf(TRANSACTIONS_TRAIN_INCPCA_700, 'TRANSACTIONS_TRAIN_INCPCA_700')
-    X = X.to_sparse(fill_value=0)
-    y = data['target']
+    import h5py
 
-    print('building model...')
+    hf = h5py.File('data/transactions-value/train_red95_601f.h5', 'r')
+    X = np.array(hf.get('transactions-value'))
+    hf.close()
 
-    # TRAIN AND EVALUATE MODEL
-    model = GradientBoostingRegressor(n_estimators=200, max_depth=4, min_samples_leaf=4, random_state=123)
-    print('RMSE: ' + str(
-        scipy.sqrt(abs(cross_val_score(model, X, y, cv=2, scoring='neg_mean_squared_log_error').mean()))))
+    X = pd.DataFrame(X)
+    print(X.shape)
+
+    pipeline = fe_pipeline()
+    X = fe_pipeline().flush(X)
+
+    y = pd.read_csv(TRANSACTIONS_TRAIN)['target']
+    y = MinMaxScaler().fit_transform(y.values.reshape(-1, 1))  # normalize output to avoid Inf
+    print(y.shape)
+
+    from sklearn.ensemble import GradientBoostingRegressor
+    from sklearn.model_selection import cross_val_score
+
+    model = GradientBoostingRegressor(n_estimators=50, max_depth=6, random_state=123)
+
+    print('RMSE: ' + str(scipy.sqrt(abs(
+        cross_val_score(model, X, y.ravel(), cv=5, scoring='neg_mean_squared_log_error', verbose=2).mean()))))
